25369316bfd08b6876a2aaa9de58a11e3708eb68
eploentham <eploentham@gmail.com>
2015-11-12 10:47:08 +0700

    581112
    bangna5

--- app/src/main/java/com/nakoyagarden/eploentham/smartfarm/MainActivity.java
+++ app/src/main/java/com/nakoyagarden/eploentham/smartfarm/MainActivity.java
@@ -1,12 +1,28 @@
 package com.nakoyagarden.eploentham.smartfarm;
 
+import android.accounts.NetworkErrorException;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     String site="";
@@ -18,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Button btnS1 = (Button)findViewById(R.id.btnSite1);
         final Button btnS2 = (Button)findViewById(R.id.btnSite2);
+        final TextView tv = (TextView)findViewById(R.id.textView);
         sfc = (SmartFarmControl) getApplicationContext();
         aaa = new test1();
         btnS1.setText("ที่บ้าน");
@@ -25,8 +42,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //sf.siteId = "1";
-                Intent s1 = new Intent(view.getContext(), MainView.class);
-                startActivityForResult(s1, 0);
+                String url="http://58.8.70.62:8080/smartfarm/testphp1.php";
+                StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                        .detectNetwork() // or .detectAll() for all detectable problems
+                        .penaltyDialog()  //show a dialog
+                                //.permitNetwork() //permit Network access
+                        .build());
+                //String url="http://www.google.com";
+                //Intent s1 = new Intent(view.getContext(), MainView.class);
+                //startActivityForResult(s1, 0);
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet httpget = new HttpGet(url);
+                HttpResponse response;
+                try {
+                    response = httpclient.execute(httpget);
+                    int status = response.getStatusLine().getStatusCode();
+                    // Examine the response status
+                    Log.i("Praeda", response.getStatusLine().toString());
+
+                    // Get hold of the response entity
+                    HttpEntity entity = response.getEntity();
+                    // If the response does not enclose an entity, there is no need
+                    // to worry about connection release
+
+                    if (entity != null) {
+
+                        // A Simple JSON Response Read
+                        InputStream instream = entity.getContent();
+                        String result= convertStreamToString(instream);
+                        // now you have the string representation of the HTML request
+                        instream.close();
+                        tv.setText(result);
+                    }
+
+
+                } catch (Exception e) {
+                    Log.e("Praeda", e.getMessage());
+                }
             }
         });
         btnS2.setText("ไร่เลิศรส");
@@ -60,5 +112,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private static String convertStreamToString(InputStream is) {
+    /*
+     * To convert the InputStream to String we use the BufferedReader.readLine()
+     * method. We iterate until the BufferedReader return null which means
+     * there's no more data to read. Each line will appended to a StringBuilder
+     * and returned as String.
+     */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 }
--- app/src/main/java/com/nakoyagarden/eploentham/smartfarm/MainView.java
+++ app/src/main/java/com/nakoyagarden/eploentham/smartfarm/MainView.java
@@ -1,12 +1,24 @@
 package com.nakoyagarden.eploentham.smartfarm;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainView extends AppCompatActivity {
     Button btnWaterView, btnSensorView, btnDashBoard;
@@ -35,6 +47,14 @@ public class MainView extends AppCompatActivity {
                 startActivityForResult(s1,0);
             }
         });
+        btnSensorView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "http://58.8.70.62:8080/smartfarm/testphp.php";
+                //String url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=80223,USA&mode=json&units=metric&cnt=7";
+                new AsyncHttpTask().execute(url);
+            }
+        });
     }
 
     @Override
@@ -57,5 +77,72 @@ public class MainView extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private String convertInputStreamToString(InputStream inputStream) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+
+        String line = "";
+        String result = "";
+
+        while((line = bufferedReader.readLine()) != null){
+            result += line;
+        }
+
+            /* Close Stream */
+        if(null!=inputStream){
+            inputStream.close();
+        }
+
+        return result;
+    }
+    public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            InputStream inputStream = null;
+
+            HttpURLConnection urlConnection = null;
+
+            Integer result = 0;
+            try {
+                /* forming th java.net.URL object */
+                URL url = new URL(params[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                 /* optional request header */
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                /* optional request header */
+                urlConnection.setRequestProperty("Accept", "application/json");
+
+                /* for Get request */
+                urlConnection.setRequestMethod("GET");
+                //urlConnection.connect();
+                int statusCode = urlConnection.getResponseCode();
+
+                /* 200 represents HTTP OK */
+                if (statusCode == 200) {
+
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+                    String response = convertInputStreamToString(inputStream);
+
+                    //parseResult(response);
+
+                    result = 1; // Successful
+
+                } else {
+                    result = 0; //"Failed to fetch data!";
+                }
+
+            } catch (Exception e) {
+                String err = e.getLocalizedMessage();
+                //Log.d( e.getLocalizedMessage());
+            }
+
+            return result; //"Failed to fetch data!";
+        }
     }
 }
